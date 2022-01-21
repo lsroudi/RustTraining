872fab07e60ab4b6b03ca55f4af5d8fcf1f18d36
Abdessamad el akroudi <lsroudi@gmail.com>
2022-01-21 02:21:58 +0100

    guess number

---
+++ guessNumber/src/main.rs
@@ -0,0 +1,38 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+   fn main() {
+
+    println!("Guess the number ...");
+
+    let secret_number = rand::thread_rng().gen_range(1, 11);
+
+    loop {
+
+
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse(){
+                            Ok(num) => num,
+                            Err(_) => continue,
+                        };
+
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Essayer plus grand!"),
+            Ordering::Greater => println!("Essayer plus petit!"),
+            Ordering::Equal => {
+            println!("Félicitation!!!!!!!");
+            break;
+            }
+    }
+    }
+
+
+}
