53b30afae3656eca65c3aa475f260f5868177cf8
Abdessamad el akroudi <lsroudi@gmail.com>
2022-01-22 01:18:53 +0100

    some test on borrowed variable

---
+++ variable/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    let x=5;
+
+    println!("La veleur du variable x est {}",x);
+
+    //error 
+    x=6;
+
+    println!("La veleur du variable x est {}",x);
+
+    // shadowing
+
+    let x = 7;
+}
